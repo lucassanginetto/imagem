eeac7026c2c82bb08c7cfd2b4cb0723769c7a0a1
Lucas Sanginetto <lucassanginetto@protonmail.com>
2025-10-08 20:43:32 -0300

    create empty PyQt5 window

---
+++ imagem.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    app.exec()
