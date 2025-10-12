3a557867a8bf58332ad0842c326860390839b9ae
Lucas Sanginetto <lucassanginetto@protonmail.com>
2025-10-11 22:29:06 -0300

    add folder opening and folder navigation

--- imagem.py
+++ imagem.py
@@ -10,21 +10,19 @@ from PyQt5.QtWidgets import (
 )
 
 
-class MainWindow(QMainWindow):
-    image_label: QLabel
-    scroll_area: QScrollArea
-    scroll_step: int = 30
-
+class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle('Image Viewer')
         self.resize(800, 600)
 
-        self.image_label = QLabel()
+        # Widgets
+
+        self.pixmap_label = QLabel()
 
         self.scroll_area = QScrollArea()
-        self.scroll_area.setWidget(self.image_label)
+        self.scroll_area.setWidget(self.pixmap_label)
         self.scroll_area.setAlignment(Qt.AlignCenter)
         self.scroll_area.horizontalScrollBar().setStyleSheet('height: 0px;')
         self.scroll_area.verticalScrollBar().setStyleSheet('width: 0px;')
@@ -33,36 +31,114 @@ class MainWindow(QMainWindow):
 
         self.statusBar().showMessage('[No image]')
 
-    def open_image(self, path: str) -> None:
-        path = os.path.abspath(path)
+        # Properties
 
-        if not os.path.exists(path):
+        self.pixmap = QPixmap()
+
+        self.folder_path = ''
+        self.folder_files: list[str] = []
+        self.file_index = -1
+
+        self.SUPPORTED_EXTS = {'.gif', '.jpeg', '.jpg', '.png', '.svg'}
+
+        self.SCROLL_STEP = 30
+
+    def change_current_file(self, index_increment: int) -> None:
+        if not self.folder_files or self.file_index == -1:
             return
-
-        self.image_label.setPixmap(QPixmap(path))
-        self.image_label.adjustSize()
-
-        self.setWindowTitle(os.path.basename(path))
-        self.statusBar().showMessage(path)
+        new_index = self.file_index + index_increment
+        if 0 <= new_index < len(self.folder_files):
+            self.file_index = new_index
+            file_path = os.path.join(
+                self.folder_path,
+                self.folder_files[self.file_index]
+            )
+            self.load_pixmap_from_file(file_path)
 
     def file_dialog_path(self) -> str:
         path, _ = QFileDialog.getOpenFileName(
             self,
             'Open Image',
             '',
-            'Images (*.gif *.jpeg *.jpg *.png *.svg)'
+            'Images (*' + ' *'.join(self.SUPPORTED_EXTS) + ')'
         )
         return path
+
+    def load_files_from_folder(self, folder_path: str) -> None:
+        files = [
+            file for file in os.listdir(folder_path)
+            if os.path.splitext(file)[1].lower() in self.SUPPORTED_EXTS
+        ]
+        files.sort()
+        self.folder_files = files
+
+    def load_pixmap_from_file(self, file_path: str) -> None:
+        self.pixmap = QPixmap(file_path)
+        if self.pixmap.isNull():
+            return
+        self.update_pixmap_label()
+
+        self.setWindowTitle(os.path.basename(file_path))
+        self.statusBar().showMessage(os.path.abspath(file_path))
+
+    def open_folder(self, folder_path: str) -> None:
+        self.load_files_from_folder(folder_path)
+
+        if not self.folder_files:
+            self.statusBar().showMessage(
+                f'[No supported files found in folder "{folder_path}"]'
+            )
+            return
+
+        file_path = os.path.join(folder_path, self.folder_files[0])
+        self.load_pixmap_from_file(file_path)
+
+        self.folder_path = folder_path
+        self.file_index = 0
+
+    def open_image(self, file_path: str) -> None:
+        folder_path = os.path.dirname(file_path)
+        self.load_files_from_folder(folder_path)
+
+        file = os.path.basename(file_path)
+        if file not in self.folder_files:
+            self.statusBar().showMessage(
+                f'[The given file at "{file_path}" isn\'t supported]'
+            )
+            return
+
+        self.load_pixmap_from_file(file_path)
+
+        self.folder_path = folder_path
+        self.file_index = self.folder_files.index(file)
+
+    def update_pixmap_label(self) -> None:
+        if self.pixmap:
+            self.pixmap_label.setPixmap(self.pixmap)
+        self.pixmap_label.adjustSize()
 
     # -------------------------------------------------------------------------
     # Event overrides
 
     def keyPressEvent(self, event: QKeyEvent) -> None:
+        modifiers = QApplication.keyboardModifiers()
 
-        if QApplication.keyboardModifiers() == Qt.ControlModifier:
+        if modifiers == Qt.ControlModifier:
             match event.key():
                 case Qt.Key_N:
                     self.statusBar().setHidden(not self.statusBar().isHidden())
+                case _:
+                    super().keyPressEvent(event)
+            return
+
+        if modifiers == Qt.ShiftModifier:
+            match event.key():
+                case Qt.Key_J:
+                    # Go to next file inside folder
+                    self.change_current_file(+1)
+                case Qt.Key_K:
+                    # Go previous file inside folder
+                    self.change_current_file(-1)
                 case _:
                     super().keyPressEvent(event)
             return
@@ -71,28 +147,28 @@ class MainWindow(QMainWindow):
             case Qt.Key_H | Qt.Key_Left:
                 self.scroll_area.horizontalScrollBar().setValue(
                     self.scroll_area.horizontalScrollBar().value()
-                    - self.scroll_step
+                    - self.SCROLL_STEP
                 )
             case Qt.Key_J | Qt.Key_Down:
                 self.scroll_area.verticalScrollBar().setValue(
                     self.scroll_area.verticalScrollBar().value()
-                    + self.scroll_step
+                    + self.SCROLL_STEP
                 )
             case Qt.Key_K | Qt.Key_Up:
                 self.scroll_area.verticalScrollBar().setValue(
                     self.scroll_area.verticalScrollBar().value()
-                    - self.scroll_step
+                    - self.SCROLL_STEP
                 )
             case Qt.Key_L | Qt.Key_Right:
                 self.scroll_area.horizontalScrollBar().setValue(
                     self.scroll_area.horizontalScrollBar().value()
-                    + self.scroll_step
+                    + self.SCROLL_STEP
                 )
 
             case Qt.Key_O:
-                path: str = self.file_dialog_path()
+                path = self.file_dialog_path()
                 if path:
-                    self.open_image(path)
+                    self.load_pixmap_from_file(path)
 
             case Qt.Key_F11:
                 if not self.isFullScreen():
@@ -110,10 +186,16 @@ class MainWindow(QMainWindow):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    window = MainWindow()
+    window = Window()
     window.show()
 
     if len(app.arguments()) > 1:
-        window.open_image(app.arguments()[1])
+        path = os.path.abspath(app.arguments()[1])
+        if os.path.isdir(path):
+            window.open_folder(path)
+        elif os.path.isfile(path):
+            window.open_image(path)
+        else:
+            window.statusBar().showMessage(f'[Invalid path "{path}"]')
 
     app.exec()
